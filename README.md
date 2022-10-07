f2ded7b8e2650cf9dfde0f4df5b1d61339f1bd5a
ooyamatakehisa <ooyamatakehisa@gmail.com>
2022-10-07 11:40:21 +0900

    update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Ocaml Interpreter
 
 ocamlインタプリタの縮小版の作成記録
+
+講義は「[プログラミング言語処理系](https://github.com/kuis-isle3sw/IoPLMaterials/)」
--- src/cui.ml
+++ src/cui.ml
@@ -3,88 +3,89 @@ open Typing
 open Syntax
 
 
-let rec read_eval_print env tyenv=
+let rec read_eval_print env tyenv =
   print_string "# ";
   flush stdout;
-  let back message = 
+  let back message =
     Printf.printf "%s" message;
     print_newline();
     read_eval_print env tyenv in
-    
+
     (*ここでtry-with文を書くことで例外処理ができる*)
-    (try 
+    (try
       let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
-      let (s,ty) = ty_decl tyenv decl in 
-      
+      let (s,ty) = ty_decl tyenv decl in
+
       (*letの連続複数回宣言やletをandで結んだ宣言などの識別子の束縛値の表示のために値を出力する部分を再帰関数として定義*)
-      let rec printrec dec = 
-      match dec with 
+      let rec printrec dec =
+      match dec with
        (*let a=3 let b=4;;のような宣言ために場合分け*)
-        Syntax.VarDecl(x,e1,top) -> 
-            let (newtyenv, ty) = ty_decl tyenv decl in 
+        Syntax.VarDecl(x,e1,top) ->
+            let (newtyenv, ty) = ty_decl tyenv decl in
             let (id, newenv, v) = eval_decl env dec in
             (*ひとつ目のlet宣言の識別子がx *)
-            Printf.printf "val %s : " x; 
-            pp_ty ty; 
-            print_string " = "; 
+            Printf.printf "val %s : " x;
+            pp_ty ty;
+            print_string " = ";
             (*そのxに束縛されている値を環境で探し出力*)
-            pp_val (Environment.lookup x newenv); 
+            pp_val (Environment.lookup x newenv);
             print_newline();
-            (*残りのlet宣言のdeclに再帰的にこのprintrec関数を適用*) 
+            (*残りのlet宣言のdeclに再帰的にこのprintrec関数を適用*)
             printrec top;
             read_eval_print newenv newtyenv
 
         (*let a=3 and b=4;;のような宣言ために場合分け*)
         | Syntax.LetAndDecl(x,e1,top) ->
-            let (newtyenv, ty) = ty_decl tyenv decl in 
+            let (newtyenv, ty) = ty_decl tyenv decl in
             let (id, newenv, v) = eval_decl env dec in
             (*ひとつ目のlet宣言の識別子がx *)
-            Printf.printf "val %s : " x; 
-            pp_ty ty; 
-            print_string " = "; 
+            Printf.printf "val %s : " x;
+            pp_ty ty;
+            print_string " = ";
             (*そのxに束縛されている値を環境で探し出力*)
-            pp_val (Environment.lookup x newenv); 
+            pp_val (Environment.lookup x newenv);
             print_newline();
-            (*残りのandで宣言された部分にに再帰的にこのprintrec関数を適*) 
+            (*残りのandで宣言された部分にに再帰的にこのprintrec関数を適*)
             printrec top;
             read_eval_print newenv newtyenv
 
         (*上のdecがLetAndDecl(x,e1,top)のときのtopの部分はlet a=3 and b=4におけるb=4のような形になっており
         LetAndRecExp(x,e,top)になっているのでその時の出力表示用に場合分け *)
-        | LetAndRecExp(x,e,top) -> 
-          let (newtyenv, ty) = ty_decl tyenv decl in 
+        | LetAndRecExp(x,e,top) ->
+          let (newtyenv, ty) = ty_decl tyenv decl in
           let (id, newenv, v) = eval_decl env dec in
             (*ひとつ目のlet宣言の識別子がx *)
-            Printf.printf "val %s : " x; 
-            pp_ty ty; 
-            print_string " = "; 
+            Printf.printf "val %s : " x;
+            pp_ty ty;
+            print_string " = ";
             (*そのxに束縛されている値を環境で探し出力*)
-            pp_val (Environment.lookup x newenv); 
+            pp_val (Environment.lookup x newenv);
             print_newline();
-            (*残りのandで宣言された部分にに再帰的にこのprintrec関数を適用*) 
+            (*残りのandで宣言された部分にに再帰的にこのprintrec関数を適用*)
             printrec top;
             read_eval_print newenv newtyenv
 
 
         (*decが普通の宣言のときの残りのケース*)
-        |_ -> 
+        |_ ->
           let (id, newenv, v) = eval_decl env decl in
-          let (newtyenv, ty) = ty_decl tyenv decl in 
+          let (newtyenv, ty) = ty_decl tyenv decl in
           Printf.printf "val %s : " id;
-          pp_ty ty; 
-          print_string " = "; 
+          pp_ty ty;
+          print_string " = ";
           pp_val v;
           print_newline();
           read_eval_print newenv newtyenv
       in
       printrec decl
+
     (*以下が例外発生時の処理文。ただしエラーをはき再び入力を受け付けるという一連の動作をbackという関数で定義してエラー発生時に再帰的に呼び出している。*)
     with Failure message -> back message
     | Parser.Error -> back "Parser Error"
     | Eval.Error message -> back message
-    | _ -> back "Error") 
-       
- 
+    | _ -> back "Error")
+
+
 
 let initial_env =
   Environment.extend "i" (IntV 1)
@@ -94,7 +95,7 @@ let initial_env =
             (Environment.extend "iii" (IntV 3)
               (Environment.extend "iv" (IntV 4) Environment.empty)))))
 
-let initial_tyenv = 
+let initial_tyenv =
   Environment.extend "i" (TyScheme([],TyInt))
-    (Environment.extend "v"  (TyScheme([],TyInt)) 
+    (Environment.extend "v"  (TyScheme([],TyInt))
       (Environment.extend "x" (TyScheme([], TyInt)) Environment.empty))
